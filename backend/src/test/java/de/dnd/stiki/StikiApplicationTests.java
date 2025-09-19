16915805690808fc536f75e8f34140cd5f9a5a5e
Tim Wortmann <86354671+TimWortmann@users.noreply.github.com>
2025-09-19 21:33:06 +0200

    Backend Initial Commit

---
+++ backend/src/test/java/de/dnd/stiki/StikiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dnd.stiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
