b882b70f934849ee079c3b06ca937b1fd5ef1565
Tim Wortmann <86354671+TimWortmann@users.noreply.github.com>
2025-09-20 12:29:07 +0200

    feat: Created backend modules

---
+++ backend/plugins/src/main/java/de/dnd/stiki/MainApplication.java
@@ -0,0 +1,12 @@
+package de.dnd.stiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
