eddabd8eec6efc709e7f9c9affd071e986bcaca4
Tim Wortmann <86354671+TimWortmann@users.noreply.github.com>
2025-09-20 21:43:41 +0200

    feat: Added building of jar files

--- backend/plugins/src/main/java/de/dnd/stiki/plugins/rest/TraitController.java
+++ backend/plugins/src/main/java/de/dnd/stiki/plugins/rest/TraitController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/trait")
+@RequestMapping("/api/trait")
 public class TraitController {
 
     @Autowired
