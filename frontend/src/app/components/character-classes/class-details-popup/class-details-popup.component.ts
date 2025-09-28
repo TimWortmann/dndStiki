c2e3b44dcb33bf160f733f6e08290fca35973d7c
Tim Wortmann <86354671+TimWortmann@users.noreply.github.com>
2025-09-28 10:05:24 +0200

    fix: Spellslots fixed

--- frontend/src/app/components/character-classes/class-details-popup/class-details-popup.component.ts
+++ frontend/src/app/components/character-classes/class-details-popup/class-details-popup.component.ts
@@ -47,7 +47,7 @@ export class ClassDetailsPopupComponent {
   }
 
   getSpellSlotsHeaderArray() : string[] {
-    return ["Level 1", "Level 2", "Level 3", "Level 4", "Level 5", "Level 6", "Level 7", "Level 8", "Level 9", "Level 10"];  
+    return ["Known Cantrips", "Level 1", "Level 2", "Level 3", "Level 4", "Level 5", "Level 6", "Level 7", "Level 8", "Level 9"];  
   }
 
 }
