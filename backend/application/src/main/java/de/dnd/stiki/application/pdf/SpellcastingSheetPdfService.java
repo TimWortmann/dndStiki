5ba0fc0e5ff191765a5a535b381b7f32879a0683
Tim Wortmann <86354671+TimWortmann@users.noreply.github.com>
2025-11-30 16:40:15 +0100

    fix: Fixed spellsheet order

--- backend/application/src/main/java/de/dnd/stiki/application/pdf/SpellcastingSheetPdfService.java
+++ backend/application/src/main/java/de/dnd/stiki/application/pdf/SpellcastingSheetPdfService.java
@@ -73,29 +73,17 @@ public class SpellcastingSheetPdfService {
         setCantripFields(character, fieldValues);
 
         List<Integer> currentSpellSlots = getCurrentSpellSlots(character);
-
-        setLevel1Fields(character, fieldValues, currentSpellSlots);
-        setLevelFields(character, fieldValues, currentSpellSlots, 2, 20, 1034, 13);
-        setLevelFields(character, fieldValues, currentSpellSlots, 3, 21, 1047, 13);
-        setLevelFields(character, fieldValues, currentSpellSlots, 4, 22, 1060, 13);
-        setLevelFields(character, fieldValues, currentSpellSlots, 5, 23, 1073, 9);
-        setLevelFields(character, fieldValues, currentSpellSlots, 6, 24, 1082, 9);
-        setLevelFields(character, fieldValues, currentSpellSlots, 7, 25, 1091, 9);
-        setLevelFields(character, fieldValues, currentSpellSlots, 8, 26, 10100, 7);
-        setLevelFields(character, fieldValues, currentSpellSlots, 9, 27, 10107, 7);
+        setSpellFields(character, fieldValues, currentSpellSlots, 1, getLevel1Fields());
+        setSpellFields(character, fieldValues, currentSpellSlots, 2, getLevel2Fields());
+        setSpellFields(character, fieldValues, currentSpellSlots, 3, getLevel3Fields());
+        setSpellFields(character, fieldValues, currentSpellSlots, 4, getLevel4Fields());
+        setSpellFields(character, fieldValues, currentSpellSlots, 5, getLevel5Fields());
+        setSpellFields(character, fieldValues, currentSpellSlots, 6, getLevel6Fields());
+        setSpellFields(character, fieldValues, currentSpellSlots, 7, getLevel7Fields());
+        setSpellFields(character, fieldValues, currentSpellSlots, 8, getLevel8Fields());
+        setSpellFields(character, fieldValues, currentSpellSlots, 9, getLevel9Fields());
 
         return fieldValues;
-    }
-
-    private void setLevel1Fields(CharacterEntity character, Map<String, String> fieldValues, List<Integer> currentSpellSlots) {
-        fieldValues.put("SlotsTotal 19", String.valueOf(getSpellSlots(currentSpellSlots, 1)));
-        List<String> level1Fields = getLevel1Fields();
-        List<SpellEntity> level1Spells = getSpellsOfLevel(character.getSpells(), 1);
-        for (int i = 0; i < level1Spells.size(); i++) {
-            if (i < level1Fields.size()) {
-                fieldValues.put(level1Fields.get(i), level1Spells.get(i).getName());
-            }
-        }
     }
 
     private void setCantripFields(CharacterEntity character, Map<String, String> fieldValues) {
@@ -106,28 +94,6 @@ public class SpellcastingSheetPdfService {
                 fieldValues.put(cantripFields.get(i), cantrips.get(i).getName());
             }
         }
-    }
-
-    private List<String> getCantripFields() {
-        List<String> cantripFields = new ArrayList<>();
-        cantripFields.add("Spells 1014");
-        cantripFields.add("Spells 1016");
-        cantripFields.add("Spells 1017");
-        cantripFields.add("Spells 1018");
-        cantripFields.add("Spells 1019");
-        cantripFields.add("Spells 1020");
-        cantripFields.add("Spells 1021");
-        cantripFields.add("Spells 1022");
-        return cantripFields;
-    }
-
-    private List<String> getSpellFields(int startingFieldNumber, int numberOfFields) {
-        List<String> spellFields = new ArrayList<>();
-        for (int i = 0; i < numberOfFields; i++) {
-            int fieldNumber = startingFieldNumber + i;
-            spellFields.add("Spells " + fieldNumber);
-        }
-        return spellFields;
     }
 
     private List<SpellEntity> getSpellsOfLevel(List<SpellEntity> allSpells, int level) {
@@ -156,59 +122,164 @@ public class SpellcastingSheetPdfService {
         return currentSpellSlots.get(level);
     }
 
-    private List<String> getLevel1Fields() {
-        List<String> level1Fields = new ArrayList<>();
-        level1Fields.add("Spells 1015");
-        level1Fields.add("Spells 1023");
-        level1Fields.add("Spells 1024");
-        level1Fields.add("Spells 1025");
-        level1Fields.add("Spells 1026");
-        level1Fields.add("Spells 1027");
-        level1Fields.add("Spells 1028");
-        level1Fields.add("Spells 1029");
-        level1Fields.add("Spells 1030");
-        level1Fields.add("Spells 1031");
-        level1Fields.add("Spells 1032");
-        level1Fields.add("Spells 1033");
-        return level1Fields;
-    }
-
-    private void setLevelFields(CharacterEntity character, Map<String, String> fieldValues, List<Integer> currentSpellSlots, int level, int slotstotalFieldNumber, int startingNumber, int numberOfFields) {
-        fieldValues.put("SlotsTotal " + slotstotalFieldNumber, String.valueOf(getSpellSlots(currentSpellSlots, level)));
-        List<String> levelFields = getSpellFields(startingNumber, numberOfFields);
+    private void setSpellFields(CharacterEntity character, Map<String, String> fieldValues, List<Integer> currentSpellSlots, int level, List<String> spellFields) {
+        int slotsTotalFieldNumber = 18 + level;
+        fieldValues.put("SlotsTotal " + slotsTotalFieldNumber, String.valueOf(getSpellSlots(currentSpellSlots, level)));
         List<SpellEntity> filteredSpells = getSpellsOfLevel(character.getSpells(), level);
         for (int i = 0; i < filteredSpells.size(); i++) {
-            if (i < levelFields.size()) {
-                fieldValues.put(levelFields.get(i), filteredSpells.get(i).getName());
+            if (i < spellFields.size()) {
+                fieldValues.put(spellFields.get(i), filteredSpells.get(i).getName());
             }
         }
-        for (String levelField : levelFields) {
-            fieldValues.put(levelField, levelField + " (Level " + level +" Field)");
-        }
+    }
+
+    private List<String> getCantripFields() {
+        List<String> cantripFields = new ArrayList<>();
+        cantripFields.add("Spells 1014");
+        cantripFields.add("Spells 1016");
+        cantripFields.add("Spells 1017");
+        cantripFields.add("Spells 1018");
+        cantripFields.add("Spells 1019");
+        cantripFields.add("Spells 1020");
+        cantripFields.add("Spells 1021");
+        cantripFields.add("Spells 1022");
+        return cantripFields;
+    }
+
+    private List<String> getLevel1Fields() {
+        List<String> spellFields = new ArrayList<>();
+        spellFields.add("Spells 1015");
+        spellFields.add("Spells 1023");
+        spellFields.add("Spells 1024");
+        spellFields.add("Spells 1025");
+        spellFields.add("Spells 1026");
+        spellFields.add("Spells 1027");
+        spellFields.add("Spells 1028");
+        spellFields.add("Spells 1029");
+        spellFields.add("Spells 1030");
+        spellFields.add("Spells 1031");
+        spellFields.add("Spells 1032");
+        spellFields.add("Spells 1033");
+        return spellFields;
+    }
+
+    private List<String> getLevel2Fields() {
+        List<String> spellFields = new ArrayList<>();
+        spellFields.add("Spells 1046");
+        spellFields.add("Spells 1034");
+        spellFields.add("Spells 1035");
+        spellFields.add("Spells 1036");
+        spellFields.add("Spells 1037");
+        spellFields.add("Spells 1038");
+        spellFields.add("Spells 1039");
+        spellFields.add("Spells 1040");
+        spellFields.add("Spells 1041");
+        spellFields.add("Spells 1042");
+        spellFields.add("Spells 1043");
+        spellFields.add("Spells 1044");
+        spellFields.add("Spells 1045");
+        return spellFields;
+    }
+
+    private List<String> getLevel3Fields() {
+        List<String> spellFields = new ArrayList<>();
+        spellFields.add("Spells 1048");
+        spellFields.add("Spells 1047");
+        spellFields.add("Spells 1049");
+        spellFields.add("Spells 1050");
+        spellFields.add("Spells 1051");
+        spellFields.add("Spells 1052");
+        spellFields.add("Spells 1053");
+        spellFields.add("Spells 1054");
+        spellFields.add("Spells 1055");
+        spellFields.add("Spells 1056");
+        spellFields.add("Spells 1057");
+        spellFields.add("Spells 1058");
+        spellFields.add("Spells 1059");
+        return spellFields;
+    }
+
+    private List<String> getLevel4Fields() {
+        List<String> spellFields = new ArrayList<>();
+        spellFields.add("Spells 1061");
+        spellFields.add("Spells 1060");
+        spellFields.add("Spells 1062");
+        spellFields.add("Spells 1063");
+        spellFields.add("Spells 1064");
+        spellFields.add("Spells 1065");
+        spellFields.add("Spells 1066");
+        spellFields.add("Spells 1067");
+        spellFields.add("Spells 1068");
+        spellFields.add("Spells 1069");
+        spellFields.add("Spells 1070");
+        spellFields.add("Spells 1071");
+        spellFields.add("Spells 1072");
+        return spellFields;
+    }
+
+    private List<String> getLevel5Fields() {
+        List<String> spellFields = new ArrayList<>();
+        spellFields.add("Spells 1074");
+        spellFields.add("Spells 1073");
+        spellFields.add("Spells 1075");
+        spellFields.add("Spells 1076");
+        spellFields.add("Spells 1077");
+        spellFields.add("Spells 1078");
+        spellFields.add("Spells 1079");
+        spellFields.add("Spells 1080");
+        spellFields.add("Spells 1081");
+        return spellFields;
+    }
+
+    private List<String> getLevel6Fields() {
+        List<String> spellFields = new ArrayList<>();
+        spellFields.add("Spells 1083");
+        spellFields.add("Spells 1082");
+        spellFields.add("Spells 1084");
+        spellFields.add("Spells 1085");
+        spellFields.add("Spells 1086");
+        spellFields.add("Spells 1087");
+        spellFields.add("Spells 1088");
+        spellFields.add("Spells 1089");
+        spellFields.add("Spells 1090");
+        return spellFields;
+    }
+
+    private List<String> getLevel7Fields() {
+        List<String> spellFields = new ArrayList<>();
+        spellFields.add("Spells 1092");
+        spellFields.add("Spells 1091");
+        spellFields.add("Spells 1093");
+        spellFields.add("Spells 1094");
+        spellFields.add("Spells 1095");
+        spellFields.add("Spells 1096");
+        spellFields.add("Spells 1097");
+        spellFields.add("Spells 1098");
+        spellFields.add("Spells 1099");
+        return spellFields;
+    }
+
+    private List<String> getLevel8Fields() {
+        List<String> spellFields = new ArrayList<>();
+        spellFields.add("Spells 10101");
+        spellFields.add("Spells 10100");
+        spellFields.add("Spells 10102");
+        spellFields.add("Spells 10103");
+        spellFields.add("Spells 10104");
+        spellFields.add("Spells 10105");
+        spellFields.add("Spells 10106");
+        return spellFields;
     }
 
     private List<String> getLevel9Fields() {
-        List<String> level9Fields = new ArrayList<>();
-        level9Fields.add("Spells 10108");
-        level9Fields.add("Spells 10107");
-        level9Fields.add("Spells 10109");
-        level9Fields.add("Spells 101010");
-        level9Fields.add("Spells 101012");
-        level9Fields.add("Spells 101013");
-        return level9Fields;
-    }
-
-    private void setLevel9Fields(CharacterEntity character, Map<String, String> fieldValues, List<Integer> currentSpellSlots) {
-        fieldValues.put("SlotsTotal 27", String.valueOf(getSpellSlots(currentSpellSlots, 9)));
-        List<String> level9Fields = getLevel9Fields();
-        List<SpellEntity> level1Spells = getSpellsOfLevel(character.getSpells(), 9);
-        for (int i = 0; i < level1Spells.size(); i++) {
-            if (i < level9Fields.size()) {
-                fieldValues.put(level9Fields.get(i), level1Spells.get(i).getName());
-            }
-        }
-        for (String levelField : level9Fields) {
-            fieldValues.put(levelField, levelField + " (Level 9 Field)");
-        }
+        List<String> spellFields = new ArrayList<>();
+        spellFields.add("Spells 10108");
+        spellFields.add("Spells 10107");
+        spellFields.add("Spells 10109");
+        spellFields.add("Spells 101010");
+        spellFields.add("Spells 101011");
+        spellFields.add("Spells 101012");
+        spellFields.add("Spells 101013");
+        return spellFields;
     }
 }
